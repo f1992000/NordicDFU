1df6eabaa9d85fde6c35913dfdfe197688b9da29
Jason Lin <>
2019-12-19 16:25:30 +0800

    import iOSDFULibrary

---
+++ NordicDFU/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  NordicDFU
+//
+//  Created by  DARFON on 2019/12/19.
+//  Copyright © 2019  DARFON. All rights reserved.
+//
+
+import UIKit
+import iOSDFULibrary
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Podfile
@@ -0,0 +1,10 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'NordicDFU' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for NordicDFU
+  pod 'iOSDFULibrary', '~> 4.6.1'
+end
